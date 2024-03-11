731b396e8bcb2c1798a468f8be66cf5256ed5542
kelvinMusandu <kelvinonyango22@gmail.com>
2024-03-11 22:47:41 +0300

    Landlord page (static)

---
+++ front_end/django-bnb-clone/app/components/ContactButton.tsx
@@ -0,0 +1,7 @@
+const ContactButton = () => {
+    return (
+        <div className="cursor-pointer mt-6 px-6 py-6 bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">Contant</div>
+    )
+}
+
+export default ContactButton;
