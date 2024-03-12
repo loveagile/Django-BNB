bf0c9e3535eb85156fedcbe89e7398e58a11fd6a
kelvinMusandu <kelvinonyango22@gmail.com>
2024-03-12 15:59:10 +0300

    Detail Page for the chat (static)

--- front_end/django-bnb-clone/app/components/inbox/Conversation.tsx
+++ front_end/django-bnb-clone/app/components/inbox/Conversation.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image';
-
 const Conversation = () => {
     return (
-        <div className='px-6 py-4 cursor pointer space-y-4 border border-gray-300 rounded-xl'>
+        <div className='px-6 py-4 cursor pointer border border-gray-300 rounded-xl'>
             <p className='mb-6 text-xl'>John Doe</p>
             <p className='text-airbnb-dark'>Go to Conversation</p>
         </div>
--- front_end/django-bnb-clone/app/inbox/page.tsx
+++ front_end/django-bnb-clone/app/inbox/page.tsx
@@ -3,8 +3,8 @@ import Conversation from '../components/inbox/Conversation';
 
 const InboxPage = () => {
     return (
-        <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <h1 className='mt-6 text-2xl'>Inbox</h1>
+        <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
+            <h1 className='my-6 text-2xl'>Inbox</h1>
             <Conversation />
             <Conversation />
             <Conversation />
