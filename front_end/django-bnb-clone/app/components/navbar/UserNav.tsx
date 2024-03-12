5c059f37a6976df369ed3105522be5fd8d01696a
kelvinMusandu <kelvinonyango22@gmail.com>
2024-03-12 16:20:25 +0300

    User Navigation Menu popup (static)

--- front_end/django-bnb-clone/app/components/navbar/UserNav.tsx
+++ front_end/django-bnb-clone/app/components/navbar/UserNav.tsx
@@ -1,7 +1,15 @@
+'use client';
+import  { useState } from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <div className="p-2 relative inline-block border rounded-full"> 
-            <button className="flex items-center">
+            <button 
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center"
+            >
             <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
@@ -10,6 +18,19 @@ const UserNav = () => {
             </svg>
 
             </button>
+
+            {isOpen && (
+                <div className='w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer'>
+                    <MenuLink
+                        onClick={() => console.log('Clicked Button')}
+                        label="Login"
+                    />
+                    <MenuLink
+                        onClick={() => console.log('Clicked Button')}
+                        label="Sign up"
+                    />
+                </div>
+            )}
         </div>
     )
 }
